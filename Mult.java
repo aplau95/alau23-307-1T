6ff9132b2736c8037594d990228c48cc8f0ded12
Andrew Lau <andrewlau@Andrews-MacBook-Pro-3.local>
2018-10-05 10:35:30 -0700

    first commit

---
+++ Mult.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+
+public class Mult {
+    private static int multiply(int x, int y){
+        return x * y;
+    }
+    public static void main(String args[]){
+        System.out.println(multiply(5, 30));
+    }
+}
